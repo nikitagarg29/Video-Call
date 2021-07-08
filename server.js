4a9215143972355eddcad7936a5d02d5761195cc
Nikita <nikitagarg2910@gmail.com>
2021-07-08 19:26:37 +0530

    added routing

--- server.js
+++ server.js
@@ -14,12 +14,24 @@ const { v4: uuidv4 } = require("uuid");
 const { ExpressPeerServer } = require("peer");
 const peerServer = ExpressPeerServer(server, { debug: true, });
 
+const userS = [], userI = [];
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use("/peerjs", peerServer);
+app.use(express.urlencoded());
+app.use(express.json());
 
 app.get("/", (req, rsp) => {
-    rsp.redirect(`/${uuidv4()}`);
+    rsp.render("index");
+});
+
+app.post('/newmeet', function(req, res) {
+    res.redirect(`/${uuidv4()}`);
+});
+
+app.post('/joinmeet', function(req, res) {
+    res.redirect(`/${req.body.roomid}`);
 });
 
 app.get("/:room", (req, rsp) => {
@@ -30,17 +42,42 @@ io.on("connection", (socket) => {
 
     socket.on("join-room", (roomId, userId) => {
         console.log("new user", userId);
+        userS.push(socket.id);
+        userI.push(userId);
         socket.join(roomId);
         socket.broadcast.to(roomId).emit("user-connected", userId);
+
+        socket.on('removeUser', (sUser, rUser)=>{
+	    	var i = userS.indexOf(rUser);
+	    	if(sUser == userI[0]){
+	    	  console.log("Removed"+rUser);
+	    	  socket.broadcast.to(roomId).emit('remove-User', rUser);
+	    	}
+	    });
+
+        socket.on('obect', (sUser, object) =>{
+            if(sUser == userI[0]){
+                socket.broadcast.to(roomId).emit('grid_obj', object);
+            }
+        });
 
         socket.on("message", (message) => {
             io.to(roomId).emit("createMessage", message);
         });
 
-        socket.on("disconnect", function () {
+        socket.on("disconnect", () => {
             console.log("user left");
-            socket.broadcast.to(roomId).emit("user-disconnected", userId);
+            var x = userS.indexOf(socket.id);
+            userS.splice(x, 1);
+            socket.broadcast.to(roomId).emit("user-disconnected", userI[x], userI);
+            userI.splice(x, 1);
         });
+
+        socket.on('seruI', () =>{
+	    	socket.emit('all_users_inRoom', userI);
+		    console.log(userI);
+	    });  
+
     });
 });
 
